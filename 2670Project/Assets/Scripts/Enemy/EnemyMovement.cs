6f8cfa571a1fadf3c2b7b662b2400ba41a9848ef
Kyle <10884088@my.uvu.edu>
2020-09-20 19:15:02 -0600

    I forgot to update my scripting... just worked on class things and a enemy move script. also some update

---
+++ 2670Project/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    GameObject player;
+    UnityEngine.AI.NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+}
