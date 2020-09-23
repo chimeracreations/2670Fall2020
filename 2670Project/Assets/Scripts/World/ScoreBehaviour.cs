878ee3b5d88ab4f744de98dd0d62b1ade5649979
Kyle <10884088@my.uvu.edu>
2020-09-22 20:29:19 -0600

    Updated some scripts and created a few that need some adjusting

---
+++ 2670Project/Assets/Scripts/World/ScoreBehaviour.cs
@@ -0,0 +1,22 @@
+﻿  
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public IntData scoreValue;
+    private Text text;
+
+    private void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        text.text = scoreValue.value.ToString();
+    }
+}
---
+++ 2670Project/Assets/Scripts/World/SpawnPointBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPointBehaviour : MonoBehaviour
+{
+    public Vector3Data vData;
+    
+    //Set the vData from the position value on start
+
+    private void OnTriggerEnter(Collider other)
+    {
+        vData.SetValueFromTransform(transform.position);
+    }
+}
