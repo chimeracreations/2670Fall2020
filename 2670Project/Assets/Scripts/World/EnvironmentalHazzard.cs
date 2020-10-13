465f3542064ec5887a99c427aa381e34094d914c
Kyle <10884088@my.uvu.edu>
2020-10-12 20:07:21 -0600

    Mix of coding and 3D animation

---
+++ 2670Project/Assets/Scripts/World/EnvironmentalHazzard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class EnvironmentalHazzard : MonoBehaviour
+{
+    private MeshRenderer mesh;
+    public Color startColor;
+    public Color nextColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        mesh.material.color = startColor;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        mesh.material.color = nextColor;
+    } 
+    private void OnTriggerExit(Collider other)
+    {
+        mesh.material.color = startColor;
+    } 
+}
