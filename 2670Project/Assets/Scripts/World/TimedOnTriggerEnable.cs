3f95dfe7edecd9caad0430c7f4c08fee3b336b45
Kyle <10884088@my.uvu.edu>
2020-11-15 21:45:29 -0700

    timed OnTrigger Enable

---
+++ 2670Project/Assets/Scripts/World/TimedOnTriggerEnable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedOnTriggerEnable : MonoBehaviour
+{
+    public float time;
+    private WaitForSeconds wfs;
+
+    private IEnumerator OnTriggerEnter(Collider other) 
+    {
+        if (other.tag == "TailStink")
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).gameObject.SetActive(true);
+            }
+            yield return wfs = new WaitForSeconds(time);
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).gameObject.SetActive(false);
+            }
+
+        }
+
+    }
+}
