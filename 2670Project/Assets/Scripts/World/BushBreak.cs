8d6e95ebed01d99e862a5f3e2a573a73ebf95bec
Kyle <10884088@my.uvu.edu>
2020-11-21 19:23:46 -0700

    platforms, falling, bush breaking

---
+++ 2670Project/Assets/Scripts/World/BushBreak.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BushBreak : MonoBehaviour
+{
+    private Animator animator;
+    private Color color1 = new Color(0.5686f, 1, 0);
+    private Color color2 = new Color(0.65098f, 0.698039f, 0.17647f);
+    private Color lerpedColor;
+    private Renderer[] colors;
+    private readonly WaitForFixedUpdate wffu = new WaitForFixedUpdate();
+    public bool isMushroom;
+
+    private void Start() 
+    {
+        animator = GetComponent<Animator>();  
+        colors = GetComponentsInChildren<Renderer>();
+        if (isMushroom) color1 = new Color(0.772549f, 0.7137f, 0.43529f);
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.tag == "TailStink")
+        {
+            animator.SetTrigger("bushDestroy");
+            StartCoroutine(ColorChange());
+        }
+
+    }
+
+    private IEnumerator ColorChange()
+    {
+        float t = 0;
+        lerpedColor = color1;
+        while (lerpedColor != color2)
+        {
+            lerpedColor = Color.LerpUnclamped(color1, color2, Mathf.Clamp(t,0,1));
+
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                colors[i].material.color = lerpedColor;
+            }
+            t += (Time.time / 300);            
+            yield return wffu;
+        }
+    
+    }
+}
---
+++ 2670Project/Assets/Scripts/World/HoleBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleBehavior : MonoBehaviour
+{
+    public PlayerData player;
+    private readonly WaitForFixedUpdate wffu = new WaitForFixedUpdate();
+    private float exitCount;
+   
+    private IEnumerator OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            exitCount = 0;
+            if (player.controller.isGrounded == false && player.movement.y < -1.5f)
+            {
+                while (player.controller.isGrounded == false && exitCount < 5)
+                {
+                    player.canControl = false;
+                    player.movement.x = 0;
+                    player.movement.y += -0.1f;
+                    player.movement.z = 0;
+                    player.controller.Move(player.movement * Time.deltaTime);
+                    exitCount += Time.deltaTime;
+                    yield return wffu;
+                }
+                    
+                player.canControl = true; 
+            }
+            else player.canControl = true;
+        }
+    }
+
+}
